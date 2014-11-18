50b2100669e75c339542fc88278d01010915e50f
Young <jdy10@jdy10-mdt-054.dbmi.pitt.edu>
2014-11-18 16:12:14 -0500

    adds preprocess_pretrain_params function. adds call to this function in run_Srbm_DAfinetune. passes in the weights and biases to DAfinetune constructor.

--- jdy_Srbm_DAfinetune.py
+++ jdy_Srbm_DAfinetune.py
@@ -11,13 +11,49 @@ from jdy_DAfinetune import DAfinetune
 from jdy_utils import load_data, save_short, save_med_pkl, save_med_npy
 
 
+def preprocess_pretrain_params(object_params):
+	'''
+	creates a list of non transposed weights and biases (in the correct 
+	order) to be used when the pretraining parameters are used to initialize a 
+	Deep Autoencoder. This makes it easier to loop through the paramateres when 
+	creating hidden layers. Note that the decoder weights returned here still 
+	need to be transposed before they are used to initialize a Deep Autoencoder.
 
+	object_params: is a list of the pretraining parameters (self.rbm_params)
+		-for a 4 layered network:
+		-object_params = [W, b, vbias, W, b, vbias, W, b, vbias, W, b, vbias]
 
-def run_Srbm_DAfinetune(pretraining_epochs=1, training_epochs=1, 
+	sample return: 	weights = [W1,W2,W3,W4,W4,W3,W2,W1]
+					biases = [b1,b2,b3,b4,vbias14,vbias3,vbias2,vbias1]
+	'''
+
+	length = len(object_params)
+	encoder_weights = []
+	decoder_weights = []
+	hbiases = []
+	vbiases = []
+	for i in xrange(0, length, 3):
+		encoder_weights.append(object_params[i])
+	for j in xrange(1, length+1, 3):
+		hbiases.append(object_params[j])
+	for k in xrange(2, length+1, 3):
+		vbiases.append(object_params[k])
+
+	### NOTE: will still need to take the transpose of the decoder_weights later
+	decoder_weights = encoder_weights[:]
+	decoder_weights.reverse()
+	weights = encoder_weights + decoder_weights
+
+	vbiases.reverse()
+	biases = hbiases + vbiases
+
+	return weights, biases
+
+def run_Srbm_DAfinetune(pretraining_epochs=1, training_epochs=5, 
 						hidden_layers_sizes=[1000, 500, 250, 30],
 						finetune_lr=0.1, pretrain_lr=0.1, 
 						k=1, batch_size=10, 
-						dataset='/Users/jon/Data/mnist/mnist.pkl.gz'):
+						dataset='/Users/jdy10/Data/mnist/mnist.pkl.gz'):
 
 	datasets = load_data(dataset)
 	train_set_x, train_set_y = datasets[0]
@@ -120,10 +156,14 @@ def run_Srbm_DAfinetune(pretraining_epochs=1, training_epochs=1,
 	# FINETUNING THE MODEL #
 	########################
 
+	#get weights and biases from pretraining to use to initialize a Deep 
+	#Autoencoder
+	weights, biases = preprocess_pretrain_params(srbm.rbm_params)
+
 	print '... building the Deep Autoencoder model'
 	# construct the Deep Autoencoder 
 	dafinetune = DAfinetune(numpy_rng=numpy_rng, n_ins=784, 
-				pretrain_params=srbm.rbm_params, 
+				W=weights, b=biases, 
 				hidden_layers_sizes=hidden_layers_sizes)
 
 
@@ -179,4 +219,4 @@ def run_Srbm_DAfinetune(pretraining_epochs=1, training_epochs=1,
 
 
 if __name__ == '__main__':
-	run_Srbm_DAfinetune()
+	srbm = run_Srbm_DAfinetune()
